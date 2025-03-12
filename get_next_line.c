b9987a8225db2e112219af5ba99e2c53d12ad89d
home <bronevikvlad@gmail.com>
2025-03-12 15:52:38 +0100

    Changes to be committed: 	new file:   a.out 	new file:   get_next_line.c 	new file:   get_next_line.h 	new file:   get_next_line_utils.c 	new file:   gnlTester 	new file:   test.txt

---
+++ get_next_line.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbronevy <vbronevy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 12:25:50 by vbronevy          #+#    #+#             */
+/*   Updated: 2025/03/12 15:07:34 by vbronevy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	static	int		i;
+	// int		size;
+	char	*c;
+	
+	c = calloc(100,1);
+	i = 0;
+	while(c[i] != '\n' || c[i] != '\0')
+	{
+		if(c[i - 1] == '\n')
+			break;
+		read(fd, &c[i], 1);
+		i++;
+	}
+	// c[i] = '\0';
+
+	return (c);
+}
+
+int main(void)
+{
+	int	fd;
+	char* str;
+
+	fd = open("test.txt", O_RDONLY);
+	str = get_next_line(fd);
+	printf("%s\n", str);
+	free(str);
+	str = get_next_line(fd);
+	printf("%s\n", str);
+	free(str);
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbronevy <vbronevy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 00:59:58 by vbronevy          #+#    #+#             */
+/*   Updated: 2025/03/12 11:52:48 by vbronevy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
